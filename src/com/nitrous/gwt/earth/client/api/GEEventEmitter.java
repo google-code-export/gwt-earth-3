288e733dcb9b7d1fab7e858418a2643dd8602103
nitrousdigital@gmail.com <nitrousdigital@gmail.com@1fceb0d7-4fe9-cb41-a728-c0c06fd72928>
2011-08-25 23:59:01 +0000

    Implemented GEEventEmitter functionality (mouse listener). Removed methods from GEEventEmitter interface as they are supposed to be EVENTS. Adding a MouseListenerDemo

--- src/com/nitrous/gwt/earth/client/api/GEEventEmitter.java
+++ src/com/nitrous/gwt/earth/client/api/GEEventEmitter.java
@@ -15,7 +15,11 @@
  */
 package com.nitrous.gwt.earth.client.api;
 
-
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.nitrous.gwt.earth.client.api.event.MouseClickListener;
+import com.nitrous.gwt.earth.client.api.event.MouseListener;
+import com.nitrous.gwt.earth.client.api.event.MouseMoveListener;
 
 /**
  * Defines when and how an event gets passed in and triggered from the Google
@@ -29,60 +33,116 @@ public class GEEventEmitter extends GESchemaObject {
 	}
 
 	/**
-	 * Triggers an event when the user clicks a location in Google Earth with
-	 * the mouse.
+	 * Register a listener that will be notified of mouse interactions with
+	 * Google Earth.
 	 * 
-	 * @param event The KmlMouseEvent
+	 * @param listener The listener to register
+	 * @return The HandlerRegistration that can be used to unregister the listener
 	 */
-	public final native void click(KmlMouseEvent event) /*-{
-		this.click(event);
+	public final HandlerRegistration addMouseClickListener(MouseClickListener listener) {
+		JavaScriptObject clickListener = doAddClickListener(listener);
+		JavaScriptObject dblClickListener = doAddDoubleClickListener(listener);
+		JavaScriptObject mouseDownListener = doAddMouseDownListener(listener);
+		JavaScriptObject mouseUpListener = doAddMouseUpListener(listener);
+		HandlerRegistration reg = new GEHandlerRegistration(this, 
+				new JavaScriptObject[]{
+					clickListener, 
+					dblClickListener, 
+					mouseDownListener, 
+					mouseUpListener
+				}, 
+				new String[]{
+					"click",
+					"dblclick",
+					"mousedown",
+					"mouseup"
+				});
+		return reg;
+	}
+	/**
+	 * Register a listener that will be notified of mouse interactions with
+	 * Google Earth.
+	 * 
+	 * @param listener The listener to register
+	 * @return The HandlerRegistration that can be used to unregister the listener
+	 */
+	public final HandlerRegistration addMouseListener(MouseListener listener) {
+		JavaScriptObject clickListener = doAddClickListener(listener);
+		JavaScriptObject dblClickListener = doAddDoubleClickListener(listener);
+		JavaScriptObject mouseOverListener = doAddMouseOverListener(listener);
+		JavaScriptObject mouseDownListener = doAddMouseDownListener(listener);
+		JavaScriptObject mouseUpListener = doAddMouseUpListener(listener);
+		JavaScriptObject mouseOutListener = doAddMouseOutListener(listener);
+		JavaScriptObject mouseMoveListener = doAddMouseMoveListener(listener);	
+		HandlerRegistration reg = new GEHandlerRegistration(this, 
+				new JavaScriptObject[]{
+					clickListener, 
+					dblClickListener, 
+					mouseOverListener, 
+					mouseDownListener, 
+					mouseUpListener, 
+					mouseOutListener,
+					mouseMoveListener
+				}, 
+				new String[]{
+					"click",
+					"dblclick",
+					"mouseover",
+					"mousedown",
+					"mouseup",
+					"mouseout",
+					"mousemove"
+				});
+		return reg;
+	}
+	private final native JavaScriptObject doAddClickListener(MouseClickListener listener) /*-{
+		var jsListener = function(event) {
+			listener.@com.nitrous.gwt.earth.client.api.event.MouseListener::onClick(Lcom/nitrous/gwt/earth/client/api/KmlMouseEvent;)(event);
+	    };
+		$wnd.google.earth.addEventListener(this, 'click', jsListener);
+		return jsListener; 
+	}-*/;
+	private final native JavaScriptObject doAddDoubleClickListener(MouseClickListener listener) /*-{
+		var jsListener = function(event) {
+			listener.@com.nitrous.gwt.earth.client.api.event.MouseListener::onDoubleClick(Lcom/nitrous/gwt/earth/client/api/KmlMouseEvent;)(event);
+	    };
+		$wnd.google.earth.addEventListener(this, 'dblclick', jsListener);
+		return jsListener; 
+	}-*/;
+	private final native JavaScriptObject doAddMouseDownListener(MouseClickListener listener) /*-{
+		var jsListener = function(event) {
+			listener.@com.nitrous.gwt.earth.client.api.event.MouseListener::onMouseDown(Lcom/nitrous/gwt/earth/client/api/KmlMouseEvent;)(event);
+	    };
+		$wnd.google.earth.addEventListener(this, 'mousedown', jsListener);
+		return jsListener; 
+	}-*/;
+	private final native JavaScriptObject doAddMouseUpListener(MouseClickListener listener) /*-{
+		var jsListener = function(event) {
+			listener.@com.nitrous.gwt.earth.client.api.event.MouseListener::onMouseUp(Lcom/nitrous/gwt/earth/client/api/KmlMouseEvent;)(event);
+	    };
+		$wnd.google.earth.addEventListener(this, 'mouseup', jsListener);
+		return jsListener; 
+	}-*/;
+	private final native JavaScriptObject doAddMouseOverListener(MouseMoveListener listener) /*-{
+		var jsListener = function(event) {
+			listener.@com.nitrous.gwt.earth.client.api.event.MouseMoveListener::onMouseOver(Lcom/nitrous/gwt/earth/client/api/KmlMouseEvent;)(event);
+	    };
+		$wnd.google.earth.addEventListener(this, 'mouseover', jsListener);
+		return jsListener; 
+	}-*/;
+	private final native JavaScriptObject doAddMouseOutListener(MouseMoveListener listener) /*-{
+		var jsListener = function(event) {
+			listener.@com.nitrous.gwt.earth.client.api.event.MouseMoveListener::onMouseOut(Lcom/nitrous/gwt/earth/client/api/KmlMouseEvent;)(event);
+	    };
+		$wnd.google.earth.addEventListener(this, 'mouseout', jsListener);
+		return jsListener; 
+	}-*/;
+	private final native JavaScriptObject doAddMouseMoveListener(MouseMoveListener listener) /*-{
+		var jsListener = function(event) {
+			listener.@com.nitrous.gwt.earth.client.api.event.MouseMoveListener::onMouseMove(Lcom/nitrous/gwt/earth/client/api/KmlMouseEvent;)(event);
+	    };
+		$wnd.google.earth.addEventListener(this, 'mousemove', jsListener);
+		return jsListener; 
 	}-*/;
 
-	/**
-	 * Triggers an event when the user double clicks a location in Google Earth with the mouse.
-	 * @param event The KmlMouseEvent
-	 */
-	public final native void dblclick(KmlMouseEvent event) /*-{
-		this.dblclick(event);
-	}-*/;
-
-	/**
-	 * Triggers an event when the user moves the mouse pointer over a location in Google Earth.
-	 * @param event The KmlMouseEvent
-	 */
-	public final native void mouseover(KmlMouseEvent event) /*-{
-		this.mouseover(event);
-	}-*/;
-
-	/**
-	 * Triggers an event when the user presses the mouse button over a location in Google Earth.
-	 * @param event The KmlMouseEvent
-	 */
-	public final native void mousedown(KmlMouseEvent event) /*-{
-		this.mousedown(event);
-	}-*/;
-
-	/**
-	 * Triggers an event when the user releases the mouse button over a location in Google Earth.
-	 * @param event The KmlMouseEvent
-	 */
-	public final native void mouseup(KmlMouseEvent event) /*-{
-		this.mouseup(event);
-	}-*/;
-
-	/**
-	 * Triggers an event when the user moves the mouse off of the object in Google Earth.
-	 * @param event The KmlMouseEvent
-	 */
-	public final native void mouseout(KmlMouseEvent event) /*-{
-		this.mouseout(event);
-	}-*/;
-
-	/**
-	 * Triggers an event when the user moves the mouse inside Google Earth.
-	 * @param event The KmlMouseEvent
-	 */
-	public final native void mousemove(KmlMouseEvent event) /*-{
-		this.mousemove(event);
-	}-*/;
 }
