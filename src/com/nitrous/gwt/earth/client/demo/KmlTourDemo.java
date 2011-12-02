97603dc74ced0055e9bfe84dc510beca4830d2ef
nitrousdigital@gmail.com <nitrousdigital@gmail.com@1fceb0d7-4fe9-cb41-a728-c0c06fd72928>
2011-12-02 01:28:53 +0000

    Adding a KML Tour demo

---
+++ src/com/nitrous/gwt/earth/client/demo/KmlTourDemo.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2011 Nick Kerr
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.nitrous.gwt.earth.client.demo;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
+import com.nitrous.gwt.earth.client.api.GELayerId;
+import com.nitrous.gwt.earth.client.api.GEPlugin;
+import com.nitrous.gwt.earth.client.api.GEPluginReadyListener;
+import com.nitrous.gwt.earth.client.api.GEVisibility;
+import com.nitrous.gwt.earth.client.api.GoogleEarth;
+import com.nitrous.gwt.earth.client.api.GoogleEarthWidget;
+import com.nitrous.gwt.earth.client.api.KmlObject;
+import com.nitrous.gwt.earth.client.api.KmlPlacemark;
+import com.nitrous.gwt.earth.client.api.KmlTour;
+import com.nitrous.gwt.earth.client.api.event.KmlLoadCallback;
+
+/**
+ * A GWT Implementation of the simple tour demo found here: http://code.google.com/apis/earth/documentation/touring.html
+ * 
+ * Loads and executes a simple KML Tour
+ * 
+ * Simple tours, with the <gx:Tour> feature as the root-level feature of the KML, 
+ * can be fetched and passed directly to GETourPlayer. The tour must be the only feature 
+ * in the KML file. If your KML doesn't meet these requirements, refer to the Complex tour example:
+ * http://code.google.com/apis/earth/documentation/touring.html#complex_tour
+ * 
+ * @author nick
+ * 
+ */
+public class KmlTourDemo implements EntryPoint {
+
+	/** 
+	 * TODO: Replace EARTH_API_KEY with a key generated for your domain.
+	 * 
+	 * To generate a key for a real deployment, visit http://code.google.com/apis/maps/signup.html 
+	 */
+	private static final String EARTH_API_KEY = "ABQIAAAAfdPr40ksX4gg7ApZBtLBdBTJY-9JfItWUGQPJaUDtRwwITcegRRUqEyZAfd8MUbdcg1_osKHEIdPMg";
+
+	private GoogleEarthWidget earth;
+	
+    public void onModuleLoad() {
+    	// Load the Earth API
+    	GoogleEarth.loadApi(EARTH_API_KEY, new Runnable(){
+			@Override
+			public void run() {
+				// start the application
+				onApiLoaded();				
+			}    		
+    	});    	
+    }
+    
+    /**
+     * The Google earth API has loaded, start the application
+     */
+    private void onApiLoaded() {
+		// construct the UI widget
+		earth = new GoogleEarthWidget();
+
+		// register a listener to be notified when the earth plug-in has loaded
+		earth.addPluginReadyListener(new GEPluginReadyListener() {
+			public void pluginReady(GEPlugin ge) {
+				// show map content once the plugin has loaded
+				loadMapContent();
+			}
+
+			public void pluginInitFailure() {
+				// failure!
+				Window.alert("Failed to initialize Google Earth Plug-in");
+			}
+		});
+
+		RootLayoutPanel.get().add(earth);
+
+		// begin loading the Google Earth Plug-in
+		earth.init();
+	}
+
+	/**
+	 * Display content on the map
+	 */
+	private void loadMapContent() {
+		// The GEPlugin is the core class and is a great place to start browsing
+		// the API
+		final GEPlugin ge = earth.getGEPlugin();
+		ge.getWindow().setVisibility(true);
+
+		// add a navigation control
+		ge.getNavigationControl().setVisibility(GEVisibility.VISIBILITY_AUTO);
+		
+		// add some layers
+		ge.enableLayer(GELayerId.LAYER_BORDERS, true);
+		ge.enableLayer(GELayerId.LAYER_ROADS, true);
+
+		// load the KML
+		String href = GWT.getHostPageBaseURL() + "kml/bounce_example.kml";
+		GoogleEarth.fetchKml(ge, href, new KmlLoadCallback(){
+			@Override
+			public void onLoaded(KmlObject feature) {
+				// play the tour
+				ge.getTourPlayer().setTour((KmlTour)feature);
+				ge.getTourPlayer().play();
+			}
+		});
+	}
+
+}
